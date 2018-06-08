8e7317dcd62d24497e70d9015487afb280b4e840
GuilleCL <39208309+guillecl-splatoon@users.noreply.github.com>
2018-06-07 20:24:26 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ ADMINS_ROLE = None
 WEREWOLF_NOTIFY_ROLE = None
 ratelimit_dict = {}
 pingif_dict = {}
-notify_me = []
+notify_me = {}
 stasis = {}
 commands = {}
 
@@ -30,9 +30,15 @@ wait_timer = datetime.now()
 
 faftergame = None
 starttime = None
-with open(NOTIFY_FILE, 'a+') as notify_file:
-    notify_file.seek(0)
-    notify_me = notify_file.read().split(',')
+#with open(NOTIFY_FILE, 'a+') as notify_file:
+#    notify_file.seek(0)
+#    notify_me = notify_file.read().split(',')
+if os.path.isfile(NOTIFY_FILE):
+    with open(NOTIFY_FILE, 'r') as notify_file:
+        notify_me = json.load(notify_file)
+else:
+    with open(NOTIFY_FILE, 'a+') as notify_file:
+        notify_file.write('{}')
 
 if os.path.isfile(STASIS_FILE):
     with open(STASIS_FILE, 'r') as stasis_file:
@@ -1412,20 +1418,24 @@ async def cmd_notify(message, parameters):
     notify = message.author.id in notify_me
     if parameters == '':
         # await send_lobby("{} PING! Parece que alguien quiere jugar Werewolves".format(WEREWOLF_NOTIFY_ROLE.mention))
-        online = ["<@{}>".format(x) for x in notify_me if is_online(x) and x not in session[1] and notify_me[x] > 0 and\
+        #online = ["<@{}>".format(x) for x in notify_me if is_online(x) and x not in session[1] and notify_me[x] > 0 and\
+        #(x in stasis and stasis[x] == 0 or x not in stasis)]
+        online = ["<@{}>".format(x) for x in notify_me.keys() if is_online(x) and x not in session[1] and notify_me[x] > 0 and\
         (x in stasis and stasis[x] == 0 or x not in stasis)]
         await client.send_message(message.channel, "{} PING! Alguien quiere jugar Werewolves!".format(''.join(online)))
     elif parameters in ['true', '+', 'yes']:
         if notify:
             await reply(message, "You are already in the notify list.")
             return
-        notify_me.append(message.author.id)
+        #notify_me.append(message.author.id)
+        notify_me[message.author.id] = 1
         await reply(message, "You will be notified by {}notify.".format(BOT_PREFIX))
     elif parameters in ['false', '-', 'no']:
         if not notify:
             await reply(message, "You are not in the notify list.")
             return
-        notify_me.remove(message.author.id)
+        #notify_me.remove(message.author.id)
+        notify_me[message.author.id] = 0
         await reply(message, "You will not be notified by {}notify.".format(BOT_PREFIX))
     else:
         await reply(message, commands['notify'][2].format(BOT_PREFIX))        
@@ -3371,8 +3381,10 @@ async def wait_timer_loop():
 async def backup_settings_loop():
     while not client.is_closed:
         print("BACKING UP SETTINGS")
+        #with open(NOTIFY_FILE, 'w') as notify_file:
+        #    notify_file.write(','.join([x for x in notify_me if x != '']))
         with open(NOTIFY_FILE, 'w') as notify_file:
-            notify_file.write(','.join([x for x in notify_me if x != '']))
+            json.dump(notify_me,notify_file)
         with open(STASIS_FILE, 'w') as stasis_file:
             json.dump(stasis, stasis_file)
         await asyncio.sleep(BACKUP_INTERVAL)
